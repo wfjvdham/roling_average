48c1d553482d779dcb62ad7534e0f9eafae37454
Wim <wfjvdham@gmail.com>
2019-02-08 15:07:50 +0100

    change zoo solution to tsibble

--- app.R
+++ app.R
@@ -1,17 +1,24 @@
 library(shiny)
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(googlesheets)
 library(lubridate)
-library(zoo)
 library(plotly)
+library(tsibble)
 
-googlesheets::gs_auth(token = "shiny_app_token.rds")
+#shiny_token <- gs_auth() 
+#saveRDS(shiny_token, "shiny_app_token.rds")
+gs_auth(token = "shiny_app_token.rds")
 sheet_key <- "1lVvc2_SD_JdwNLcrNgTfDg0n7w-sc9nSBhE_fDn5_IM"
 ss <- gs_key(sheet_key)
 
 sheet_data <- gs_read(ss) %>%
   filter(!is.na(last_30days)) %>%
-  mutate(date = dmy(date))
+  mutate(
+    date = dmy(date),
+    id = 1,
+    exersice = as.numeric(!is.na(last_30days))
+  )
 
 ui <- fluidPage(
   titlePanel("Fitness Progress"),
@@ -21,16 +28,18 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$plot <- renderPlot({
-    tibble(
-      date = seq(min(sheet_data$date) - years(1), today(), by = "1 day")
-    ) %>%
-      merge(sheet_data, by = "date", all.x = T) %>%
-      mutate(
-        exersice = as.numeric(!is.na(last_30days)),
-        last_month_mean = rollmean(exersice, 30, fill = NA, align = "right") * 30,
-        last_year_mean = rollmean(exersice, 365, fill = NA, align = "right") * 30
+    sheet_data %>%
+      as_tsibble(
+        index = date,
+        key = id(id)    
       ) %>%
-      filter(date >= start_date) %>%
+      fill_gaps(.full = TRUE) %>%
+      mutate(
+        last_month_mean = slide_dbl(
+          exersice, sum, .size = 31, na.rm = TRUE),
+        last_year_mean = slide_dbl(
+          exersice, sum, .size = 365, na.rm = TRUE) / 12,
+      ) %>%
       ggplot() +
       geom_step(aes(date, last_month_mean), color = "green") +
       geom_step(aes(date, last_year_mean), color = "blue") +
@@ -39,8 +48,8 @@ server <- function(input, output) {
       scale_y_continuous(breaks = seq(0, 20, 1), minor_breaks = seq(0, 20, 1),
                          position = "right") + 
       theme_bw() +
-      labs(title = "Monthly average per year and month",
-           y = "Average")
+      labs(title = "Last month actual and yearly average",
+           y = "Times per Month")
   })
 }
 
